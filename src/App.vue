a2ed9a30206d5b20d375b3232da18562f79659df
GiordanoDiego <diegomgiordano96@gmail.com>
2024-01-22 11:55:26 +0100

    template con vite - vue -sass - bootstrapp

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+/* Come faccio ad utilizzare un componente dentro un altro?
+        Si seguono 3 passaggi:
+        1) Importazione del componente
+        2) Dichiarazione del componente all'interno dell'oggetto
+        3) Utilizzo del componente    
+*/
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+    data(){
+        return{
+            
+        }
+    },
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    },
+    methods: {
+
+    }
+}
+
+
+</script>
+
+<template>
+    <AppHeader/>
+    <AppMain/>
+    <AppFooter/>
+</template>
+
+<style lang="scss"> //tolgo scoped per applicare il reset al body
+      @use "./assets/SCSS/main.scss" as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,37 @@
+<script>
+/* Come faccio ad utilizzare un componente dentro un altro?
+        Si seguono 3 passaggi:
+        1) Importazione del componente
+        2) Dichiarazione del componente all'interno dell'oggetto
+        3) Utilizzo del componente    
+*/
+
+export default {
+    data(){
+        return{
+            
+        }
+    },
+    components: {
+       
+    },
+   
+    methods: {
+
+    }
+}
+
+
+</script>
+
+<template>
+    <main>
+        <div class="my_container">
+            main
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    @use'../assets/SCSS/main.scss';
+</style>
