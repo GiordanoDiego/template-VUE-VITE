fd9186436b89872e9ecf704537228a2477bc6e01
GiordanoDiego <diegomgiordano96@gmail.com>
2024-01-22 15:06:14 +0100

    added store js

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,13 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+// import axios form 'axios';
+import {store} from './store.js'
 
 export default {
     data(){
         return{
-            
+            store,
         }
     },
     components: {
